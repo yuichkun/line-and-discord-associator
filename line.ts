5675505d2e18989e8a26d8ed23ef331da697aab3
yuichkun <yuichkun@gmail.com>
2024-04-05 17:34:57 +0900

    add sendLineMessage function

---
+++ line.ts
@@ -0,0 +1,28 @@
+export async function sendLineMessage(name: string, text: string) {
+  const webhookURL = "https://api.line.me/v2/bot/message/push";
+
+  const LINE_CHANNEL_ACCESS_TOKEN = process.env.LINE_CHANNEL_ACCESS_TOKEN;
+  if (!LINE_CHANNEL_ACCESS_TOKEN) {
+    throw new Error("LINE_CHANNEL_ACCESS_TOKEN is not set");
+  }
+
+  const response = await fetch(webhookURL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${LINE_CHANNEL_ACCESS_TOKEN}`,
+    },
+    body: JSON.stringify({
+      to: process.env.LINE_GROUP_ID,
+      messages: [
+        {
+          type: "text",
+          text: `${name} 「${text}」`,
+        },
+      ],
+    }),
+  });
+  console.log(response);
+}
+
+await sendLineMessage("yogo", "こんにちは");
